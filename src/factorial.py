d62bea7c6492462063cfa41f1eb5b6de3c739fb7
junseok <junseok0913@kakao.com>
2025-02-11 13:44:36 +0900

    액션 테스트

--- src/factorial.py
+++ src/factorial.py
@@ -3,8 +3,8 @@ def factorial(n: int) -> int:
     n!을 계산해주는 함수
     n이 음수일 경우 예외(ValueError)를 발생
     """
-    if n < 0:
-        raise ValueError("음수에 대해서는 팩토리얼을 구할 수 없습니다.")
+    #if n < 0:
+    #    raise ValueError("음수에 대해서는 팩토리얼을 구할 수 없습니다.")
     if n == 0:
         return 1
 
